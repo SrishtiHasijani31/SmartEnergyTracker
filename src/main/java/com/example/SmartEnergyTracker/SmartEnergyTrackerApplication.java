b5a843d788ce00b14c20b4aaffec8bdd73078b80
SrishtiHasijani31 <srishti.hasijani31@gmail.com>
2025-07-17 19:04:01 +0100

    Project created

--- src/main/java/com/example/SmartEnergyTracker/SmartEnergyTrackerApplication.java
+++ src/main/java/com/example/SmartEnergyTracker/SmartEnergyTrackerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SmartEnergyTrackerApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Application created");
 		SpringApplication.run(SmartEnergyTrackerApplication.class, args);
 	}
 
