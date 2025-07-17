ef637acbaed23c21692908809f1e62288705fb4f
SrishtiHasijani31 <srishti.hasijani31@gmail.com>
2025-07-17 19:02:28 +0100

    Project created

---
+++ src/test/java/com/example/SmartEnergyTracker/SmartEnergyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartEnergyTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEnergyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
